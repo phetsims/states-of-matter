96724e3dad27e0e70a3b8f89beb1a44aab38a57a
Siddhartha Chinthapally <siddhartha@zessta.com>
2014-12-02 10:13:49 +0530

    Updated the particle container node. It no longer "extends" canvas and instead has place holder layers for particles and particle foreground and background layers.

--- js/common/view/ParticleContainerNode.js
+++ js/common/view/ParticleContainerNode.js
@@ -16,8 +16,17 @@ define( function( require ) {
   var Shape = require( 'KITE/Shape' );
   var Path = require( 'SCENERY/nodes/Path' );
   var StatesOfMatterConstants = require( 'STATES_OF_MATTER/common/StatesOfMatterConstants' );
+  var DialGaugeNode = require( 'STATES_OF_MATTER/common/view/DialGaugeNode' );
+  var PointingHandNode = require( 'STATES_OF_MATTER/common/view/PointingHandNode' );
   var LinearGradient = require( 'SCENERY/util/LinearGradient' );
-  var CanvasNode = require( 'SCENERY/nodes/CanvasNode' );
+  var GaugeNode = require( 'SCENERY_PHET/GaugeNode' );
+  var ParticleCanvasNode = require( 'STATES_OF_MATTER/common/view/ParticleCanvasNode' );
+
+  // constants
+  var PRESSURE_GAUGE_Y_OFFSET = -3000;
+  var PRESSURE_METER_X_OFFSET_PROPORTION = 0.80;
+  // Maximum value expected for pressure, in atmospheres.
+  var MAX_PRESSURE = 200;
 
   /**
    * Main constructor.
@@ -27,23 +36,27 @@ define( function( require ) {
    * @param {Object} [options]
    * @constructor
    */
-  function ParticleContainerNode( model, modelViewTransform, options ) {
+  function ParticleContainerNode( model, modelViewTransform, options, volumeControlEnabled, pressureGaugeEnabled ) {
 
     this.model = model;
     this.modelViewTransform = modelViewTransform;
     this.containmentAreaWidth = StatesOfMatterConstants.CONTAINER_BOUNDS.width;
     this.containmentAreaHeight = StatesOfMatterConstants.CONTAINER_BOUNDS.height;
-    CanvasNode.call( this, options );
-    this.invalidatePaint();
+
+    Node.call( this );
+
     var preParticleLayer = new Node();
     var particleLayer = new Node();
     var postParticleLayer = new Node( {opacity: 0.8} );
 
+    //which contain handle node(container open/close node) and used to set pressure of container with finger
+    this.containerLid = new Node();
     this.addChild( preParticleLayer );
     this.addChild( particleLayer );
     this.addChild( postParticleLayer );
 
-    var openEllipse = new Path( new Shape().ellipticalArc( 125, 2, 25, 125, Math.PI / 2, 0, 2 * Math.PI, false ).close(), {
+    var openEllipse = new Path( new Shape()
+      .ellipticalArc( 125, 2, 25, 125, Math.PI / 2, 0, 2 * Math.PI, false ).close(), {
       lineWidth: '2',
       stroke: '#7E7E7E',
       fill: new LinearGradient( 0, 0, 100, 0 )
@@ -52,7 +65,8 @@ define( function( require ) {
         .addColorStop( 0.5, '# 757575' )
         .addColorStop( 0.6, '#696969' )
     } );
-    var closeEllipse = new Path( new Shape().ellipticalArc( 125, StatesOfMatterConstants.VIEW_CONTAINER_HEIGHT, 125,
+    var closeEllipse = new Path( new Shape()
+      .ellipticalArc( 125, StatesOfMatterConstants.VIEW_CONTAINER_HEIGHT, 125,
       25, 0, 0, Math.PI, false ).close(), {
       lineWidth: '2',
       stroke: '#7E7E7E',
@@ -62,7 +76,6 @@ define( function( require ) {
         .addColorStop( 0.5, '# 757575' )
         .addColorStop( 0.6, '#696969' )
     } );
-
 
     var outerMostNode = new Path( new Shape()
       .moveTo( 0, 5 )
@@ -84,20 +97,10 @@ define( function( require ) {
         .addColorStop( 0.5, '# 757575' )
         .addColorStop( 0.6, '#696969' )
     } );
+    this.containerLid.addChild( openEllipse );
     postParticleLayer.addChild( outerMostNode );
-    postParticleLayer.addChild( openEllipse );
+    postParticleLayer.addChild( this.containerLid );
     postParticleLayer.addChild( closeEllipse );
-    /*var innerNode = new Path( new Shape()
-      .moveTo( 50, 50 )
-      .lineTo( 50, StatesOfMatterConstants.VIEW_CONTAINER_HEIGHT - 5 )
-      .lineTo( StatesOfMatterConstants.VIEW_CONTAINER_WIDTH - 50, StatesOfMatterConstants.VIEW_CONTAINER_HEIGHT - 5 )
-      .lineTo( StatesOfMatterConstants.VIEW_CONTAINER_WIDTH - 50, 50 )
-      .lineTo( 50, 50 )
-      .close(), {
-      lineWidth: '2',
-      stroke: '#7E7E7E',
-      fill: 'black'
-    } );*/
 
     var middleNode = new Path( new Shape()
       .moveTo( 25, 25 )
@@ -122,40 +125,106 @@ define( function( require ) {
     } );
 
     postParticleLayer.addChild( middleNode );
+    this.middleContainerLayer = new Node();
+    // this.middleContainerLayer.bottom=this.top;
+    this.addChild( this.middleContainerLayer );
+    //this.pressureMeter;
+    if ( volumeControlEnabled ) {
 
+      // Add the finger for pressing down on the top of the container.
+      // responsible for positioning itself later based on user interaction.
 
-    model.reset();
-    this.modelViewTransform = modelViewTransform;
+      var fingerNode = new PointingHandNode( model );
+      this.addChild( fingerNode );
+      fingerNode.bottom = this.containerLid.top;
+
+      // todo: Add the lid and handle
+      // Add the handle to the lid.
+      //  var handleNode = new HandleNode(50, 100, Color.YELLOW);
+      //  handleNode.rotate(Math.PI / 2);
+      //  handleNode.setOffset((m_containerLid.getWidth() / 2) + (handleNode.getFullBoundsReference().width / 2), 0);
+      //m_containerLid.addChild(handleNode);
+    }
+    if ( pressureGaugeEnabled ) {
+      // Add the pressure meter.
+      // this.pressureMeter = new DialGaugeNode(PRESSURE_GAUGE_WIDTH_PROPORTION * m_containmentAreaWidth, StatesOfMatterStrings.PRESSURE_GAUGE_TITLE, 0, MAX_PRESSURE, StatesOfMatterStrings.PRESSURE_GAUGE_UNITS);
+
+      this.pressureMeter = new DialGaugeNode( model, 20 );
+      this.pressureMeter.setElbowEnabled( true );
+      this.middleContainerLayer.addChild( this.pressureMeter );
+      this.updatePressureGauge();
+      this.pressureMeterElbowOffset = this.pressureMeter.getCenterY();
+    }
+    var particleContainerNode = this;
+    model.targetContainerHeightProperty.link( function( containerHeight ) {
+      particleContainerNode.updatePressureGauge();
+    } );
     this.mutate( options );
   }
 
-  return inherit( CanvasNode, ParticleContainerNode, {
+  return inherit( Node, ParticleContainerNode, {
 
     /**
-     * Paints the particles on the canvas node.
-     * @param {CanvasContextWrapper} wrapper
+     * Update the position and other aspects of the gauge so that it stays
+     * connected to the lid or moves as it should when the container
+     * explodes.
+     * @private
      */
-    paintCanvas: function( wrapper ) {
-      var context = wrapper.context;
-      var particle, i;
+    updatePressureGauge: function() {
+      if ( this.pressureMeter != null ) {
+        var containerRect = this.model.getParticleContainerRect();
+        if ( !this.model.getContainerExploded() ) {
+          if ( this.pressureMeter.getRotation() != 0 ) {
+            this.pressureMeter.setRotation( 0 );
+          }
+          //this.pressureMeter.setTranslation( -this.pressureMeter.width * PRESSURE_METER_X_OFFSET_PROPORTION, PRESSURE_GAUGE_Y_OFFSET );
 
-      // paint the regular particles
-      for ( i = 0; i < this.model.particles.length; i++ ) {
-        particle = this.model.particles.get( i );
-        context.fillStyle = particle.color;
-        context.beginPath();
-        // console.log( particle.positionProperty.x );
-        context.arc( this.modelViewTransform.modelToViewX( particle.positionProperty.get().x ),
-          this.modelViewTransform.modelToViewY( particle.positionProperty.get().y ),
-          this.modelViewTransform.modelToViewDeltaX( particle.radius ), 0, 2 * Math.PI, true );
-        context.fill();
+          this.pressureMeter.setElbowHeight( StatesOfMatterConstants.PARTICLE_CONTAINER_INITIAL_HEIGHT -
+                                             containerRect.height - this.pressureMeterElbowOffset );
+        }
+        else {
+          // The container is exploding, so spin and move the gauge.
+          this.pressureMeter.setRotation( -Math.PI / 20 );
+          this.pressureMeter.setTranslation( -this.pressureMeter.width * PRESSURE_METER_X_OFFSET_PROPORTION,
+              PRESSURE_GAUGE_Y_OFFSET - this.model.getParticleContainerHeight() );
+        }
       }
-
-
     },
 
-    step: function() {
-      this.invalidatePaint();
+    /**
+     * Handle a notification that the container size has changed.
+     * @private
+     */
+    handleContainerSizeChanged: function() {
+      // changed.
+      var containerHeight = this.model.getParticleContainerHeight();
+      if ( !this.model.getContainerExploded() ) {
+        if ( this.containerLid.getRotation() != 0 ) {
+          this.containerLid.setRotation( 0 );
+        }
+        this.containerLid.setTranslation( (this.containmentAreaWidth - this.containerLid.width) / 2,
+            this.containmentAreaHeight - containerHeight - (m_containerLid.getFullBoundsReference().height / 2) +
+            LID_POSITION_TWEAK_FACTOR );
+      }
+      else {
+        // blown off the top of the container.
+        this.containerLid.rotateAboutPoint( this.rotationAmount,
+            (this.containmentAreaWidth / 2) / this.containerLid.getScale(), 0 );
+        var centerPosY = this.containmentAreaHeight - containerHeight - (this.containerLid.height / 2) +
+                         LID_POSITION_TWEAK_FACTOR;
+        var currentPosY = this.containerLid.y;
+        var newPosX = this.containerLid.x;
+        var newPosY;
+        if ( currentPosY > centerPosY ) {
+          newPosY = centerPosY;
+        }
+        else {
+          newPosY = currentPosY;
+        }
+        this.containerLid.setTranslation( newPosX, newPosY );
+      }
+      this.updatePressureGauge();
     }
+
   } );
 } );
