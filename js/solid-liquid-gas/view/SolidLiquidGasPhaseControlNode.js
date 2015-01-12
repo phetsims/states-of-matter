979771bcccea24b68dbfb99aeba3bb113535d940
Siddhartha Chinthapally <siddhartha@zessta.com>
2015-01-12 12:56:47 +0530

    Draw icons on buttons using shapes instead of images.

--- js/solid-liquid-gas/view/SolidLiquidGasPhaseControlNode.js
+++ js/solid-liquid-gas/view/SolidLiquidGasPhaseControlNode.js
@@ -14,19 +14,21 @@ define( function( require ) {
   var Panel = require( 'SUN/Panel' );
   var Text = require( 'SCENERY/nodes/Text' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var Shape = require( 'KITE/Shape' );
+  var Path = require( 'SCENERY/nodes/Path' );
   var Node = require( 'SCENERY/nodes/Node' );
   var RadioButtonGroup = require( 'SUN/buttons/RadioButtonGroup' );
-  var Image = require( 'SCENERY/nodes/Image' );
-
-  //images
-  var iceImage = require( 'image!STATES_OF_MATTER/ice-cube.png' );
-  var liquidInCupImage = require( 'image!STATES_OF_MATTER/liquid-in-cup.png' );
-  var gasImage = require( 'image!STATES_OF_MATTER/gas.png' );
+  var Circle = require( 'SCENERY/nodes/Circle' );
+  var RadialGradient = require( 'SCENERY/util/RadialGradient' );
 
   //strings
   var solidString = require( 'string!STATES_OF_MATTER/solid' );
   var liquidString = require( 'string!STATES_OF_MATTER/liquid' );
   var gasString = require( 'string!STATES_OF_MATTER/gas' );
+
+  var SOLID_STATE = 1;
+  var LIQUID_STATE = 2;
+  var GAS_STATE = 3;
 
   /**
    *
@@ -46,7 +48,7 @@ define( function( require ) {
     }, options );
 
     Node.call( this );
-    var textOptions = {font: new PhetFont( 14 ), fill: 'black', fontWeight: 'bold'};
+    var textOptions = { font: new PhetFont( 14 ), fill: 'black', fontWeight: 550};
 
     // itemSpec describes the pieces that make up an item in the control panel,
     // conforms to the contract: { label: {Node}, icon: {Node} (optional) }
@@ -74,9 +76,9 @@ define( function( require ) {
 
 
     var radioButtonContent = [
-      { value: 1, node: createItem( solid ) },
-      { value: 2, node: createItem( liquid ) },
-      { value: 3, node: createItem( gas ) }
+      { value: SOLID_STATE, node: createItem( solid ) },
+      { value: LIQUID_STATE, node: createItem( liquid ) },
+      { value: GAS_STATE, node: createItem( gas ) }
     ];
 
     var radioButtonGroup = new RadioButtonGroup( stateProperty, radioButtonContent, {
@@ -85,7 +87,7 @@ define( function( require ) {
       cornerRadius: 5,
       baseColor: '#FFECCF',
       disabledBaseColor: '#FFECCF',
-      selectedLineWidth: 3,
+      selectedLineWidth: 1,
       selectedStroke: '#FFECCF',
       deselectedLineWidth: 0,
       deselectedContentOpacity: 1,
@@ -103,20 +105,98 @@ define( function( require ) {
     this.mutate( this.options );
   }
 
-  var imageScale = 0.8;
-  //Create an icon for the solid button
+  var imageScale = 0.5;
   var createSolidIcon = function() {
-    return new Image( iceImage, {scale: imageScale} );
-  };
 
-  //Create an icon for the liquid button
+    var frontShape = new Path( new Shape()
+        .moveTo( 2, 6 )
+        .lineTo( 2, 39 )
+        .lineTo( 17, 49 )
+        .lineTo( 17, 14 )
+        .close(), { stroke: '#15597F', fill: '#15597F'}
+    );
+    var topShape = new Path( new Shape()
+        .moveTo( 2, 6 )
+        .lineTo( 33, 0 )
+        .lineTo( 49, 9 )
+        .lineTo( 17, 14 )
+        .close(), {  stroke: '#2874B2', fill: '#2874B2'}
+    );
+    var sidShape = new Path( new Shape()
+        .moveTo( 17, 14 )
+        .lineTo( 17, 49 )
+        .lineTo( 50, 44 )
+        .lineTo( 49, 9 )
+        .close(), { stroke: '#2990DF', fill: '#2990DF'}
+    );
+    return new Node( { children: [ frontShape, topShape, sidShape], scale: imageScale} );
+  };
   var createLiquidIcon = function() {
-    return new Image( liquidInCupImage, {scale: imageScale} );
-  };
 
-  //Create an icon for the gas button
+    // bucket shape
+    var outerShape = new Path( new Shape()
+        .moveTo( 2, 7 )
+        .quadraticCurveTo( 20, 2, 46, 7 )
+        .lineTo( 41, 50 )
+        .quadraticCurveTo( 21, 52, 8, 50 )
+        .close(), { lineWidth: 1.2, stroke: '#7B7B7B'}
+    );
+    // liquid shape
+    var innerShape = new Path( new Shape()
+        .moveTo( 6, 24 )
+        .quadraticCurveTo( 21, 20, 45, 24 )
+        .lineTo( 41, 50 )
+        .quadraticCurveTo( 21, 52, 8, 50 )
+        .close(), { lineWidth: 1.2, stroke: '#7B7B7B', fill: '#3DACFF'}
+    );
+
+    return new Node( { children: [ outerShape, innerShape ], scale: imageScale} );
+  };
   var createGasIcon = function() {
-    return new Image( gasImage, {scale: imageScale} );
+
+    var largeCircleRadius = 15;
+    var mediumCircleRadius = 10;
+    var smallCircleRadius = 5;
+    var circle1 = new Circle( largeCircleRadius, { stroke: '#3DACFF', fill: new RadialGradient( 0, 0, 0, 0, 0, 15 )
+      .addColorStop( 0, '#6BBFF5' )
+      .addColorStop( 1, '#3DACFF' )} );
+    var circle2 = new Circle( largeCircleRadius, { stroke: '#3DACFF', fill: new RadialGradient( 0, 0, 0, 0, 0, 15 )
+      .addColorStop( 0, '#6BBFF5' )
+      .addColorStop( 1, '#3DACFF' )} );
+    var circle3 = new Circle( 10, { stroke: '#3DACFF', fill: new RadialGradient( 0, 0, 0, 0, 0, 10 )
+      .addColorStop( 0, '#6BBFF5' )
+      .addColorStop( 1, '#3DACFF' )} );
+    var circle4 = new Circle( mediumCircleRadius, { stroke: '#3DACFF',
+      fill: new RadialGradient( 0, 0, 0, 0, 0, mediumCircleRadius )
+        .addColorStop( 0, '#6BBFF5' )
+        .addColorStop( 1, '#3DACFF' )} );
+    var circle5 = new Circle( 8, { stroke: '#3DACFF', fill: new RadialGradient( 0, 0, 0, 0, 0, 8 )
+      .addColorStop( 0, '#6BBFF5' )
+      .addColorStop( 1, '#3DACFF' )} );
+
+    circle2.top = circle1.bottom - largeCircleRadius;
+    circle2.left = circle1.right - 20;
+    circle3.top = circle1.top;
+    circle3.left = circle1.right - 5;
+    circle4.top = circle3.bottom - 5;
+    circle4.left = circle3.right - mediumCircleRadius;
+    circle5.top = circle1.bottom - 5;
+    circle5.right = circle1.left + 15;
+
+    var circle6 = new Circle( smallCircleRadius, { stroke: '#3DACFF', fill: '#6BBFF5'} );
+    var circle7 = new Circle( smallCircleRadius, { stroke: '#3DACFF', fill: '#6BBFF5'} );
+    var circle8 = new Circle( smallCircleRadius, { stroke: '#3DACFF', fill: '#6BBFF5'} );
+    var circle9 = new Circle( smallCircleRadius, { stroke: '#3DACFF', fill: '#6BBFF5'} );
+    var circle10 = new Circle( smallCircleRadius, { stroke: '#3DACFF', fill: '#6BBFF5'} );
+    circle6.left = circle1.right + 40;
+    circle7.right = circle6.left;
+    circle8.left = circle6.right;
+    circle9.top = circle6.bottom - 2;
+    circle10.bottom = circle6.top + 2;
+    circle9.left = circle7.right - smallCircleRadius;
+    circle10.right = circle8.left - smallCircleRadius;
+    return new Node( { children: [ circle1, circle2, circle3, circle4, circle5, circle6, circle7,
+                                   circle8, circle9, circle10 ], scale: imageScale } );
   };
   return inherit( Node, SolidLiquidGasPhaseControlNode );
 } );
