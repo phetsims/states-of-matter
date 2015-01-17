0970ba3b54925eff1691f5c26dc657c79eb0a18c
Siddhartha Chinthapally <siddhartha@zessta.com>
2015-01-17 19:28:47 +0530

    Code refactor. Match the barometer node size and position with the mock. See #11

--- js/common/view/DialGaugeNode.js
+++ js/common/view/DialGaugeNode.js
@@ -30,13 +30,13 @@ define( function( require ) {
   var CONNECTOR_LENGTH_PROPORTION = 1;
 
   // Width of connector wrt overall diameter.
-  var CONNECTOR_WIDTH_PROPORTION = 0.15;
+  var CONNECTOR_WIDTH_PROPORTION = 0.2;
 
   /**
-   * @param {MultipleParticleModel} model
+   * @param {MultipleParticleModel} multipleParticleModel - model  of the simulation
    * @constructor
    */
-  function DialGaugeNode( model ) {
+  function DialGaugeNode( multipleParticleModel ) {
 
     var dialGaugeNode = this;
     Node.call( this );
@@ -44,11 +44,9 @@ define( function( require ) {
     this.elbowEnabled = false;
     this.elbowHeight = 0;
 
-    this.dialComponentsNode = new Node();
-    this.dialComponentsNode.setTranslation( 0, 0 );
-    var gaugeNode = new GaugeNode( model.pressureProperty, pressureString,
-      { min: 0, max: 200 }, { scale: 0.5 } );
 
+    var gaugeNode = new GaugeNode( multipleParticleModel.pressureProperty, pressureString,
+      { min: 0, max: 200 }, { scale: 0.5, radius: 80 } );
 
     // Add the textual readout display.
     this.textualReadoutBoxShape = new Rectangle( 0, 0, 80, 15, 2, 2, { fill: 'white', stroke: 'black' } );
@@ -58,40 +56,37 @@ define( function( require ) {
     this.textualReadout.center = this.textualReadoutBoxShape.center;
 
     this.connector = new Path( null, {
-      lineWidth: 8,
+      lineWidth: 10,
       stroke: new LinearGradient( 0, 0, 60, 60 )
         .addColorStop( 0, '#D8D7D8' )
         .addColorStop( 0.4, '#E1E2E3' )
         .addColorStop( 0.8, '#D5D7D8' )
         .addColorStop( 0.9, '#E2E3E4' )
     } );
-    var roundedRectangle = new Rectangle( 0, 0, 30, 25, 2, 2, {
+    this.roundedRectangle = new Rectangle( 0, 0, 30, 25, 2, 2, {
       fill: new LinearGradient( 0, 0, 0, 25 )
         .addColorStop( 0, '#5F6973' )
         .addColorStop( 0.6, '#F0F1F2' )
     } );
 
-    roundedRectangle.centerY = gaugeNode.centerY;
-    roundedRectangle.left = gaugeNode.right - 10;
-    this.dialComponentsNode.addChild( this.connector );
-    this.dialComponentsNode.addChild( roundedRectangle );
-    this.dialComponentsNode.addChild( gaugeNode );
-    this.dialComponentsNode.addChild( this.textualReadoutBoxShape );
-    this.dialComponentsNode.addChild( this.textualReadout );
-    this.connector.centerY = roundedRectangle.centerY;
-    this.connector.x = roundedRectangle.centerX + roundedRectangle.width / 2;
+    this.roundedRectangle.centerY = gaugeNode.centerY;
+    this.roundedRectangle.left = gaugeNode.right - 10;
+    var dialComponentsNode = new Node( {
+      children: [ this.connector, this.roundedRectangle, gaugeNode,
+        this.textualReadoutBoxShape, this.textualReadout ]
+    } );
 
     // Set the initial value.
-    model.pressure = model.getPressureInAtmospheres();
-    model.pressureProperty.link( function() {
-      dialGaugeNode.textualReadout.setText( model.getPressureInAtmospheres().toFixed( 2 ) + ' ' + pressureUnitsInAtm );
+    multipleParticleModel.pressure = multipleParticleModel.getPressureInAtmospheres();
+    multipleParticleModel.pressureProperty.link( function() {
+      dialGaugeNode.textualReadout.setText( multipleParticleModel.getPressureInAtmospheres().toFixed( 2 ) + ' ' + pressureUnitsInAtm );
       dialGaugeNode.textualReadout.center = dialGaugeNode.textualReadoutBoxShape.center;
     } );
 
     this.updateConnector();
 
     // Now add the dial as a child of the main node.
-    this.addChild( this.dialComponentsNode );
+    this.addChild( dialComponentsNode );
 
   }
 
@@ -123,7 +118,8 @@ define( function( require ) {
       if ( !this.elbowEnabled ) {
         var connectorShape = new Shape.rect( 0, 0, length, width );
         this.connector.setShape( connectorShape );
-        // this.connector.setTranslation( this.dialComponentsNode.width * 0.9, this.diameter / 2 - width / 2 );
+        this.connector.setTranslation( this.roundedRectangle.centerX + this.roundedRectangle.width / 2,
+          ( this.roundedRectangle.centerY - width / 2) );
       }
       else {
         this.connectorPath.moveTo( 0, 0 );
@@ -162,4 +158,3 @@ define( function( require ) {
     }
   } );
 } );
-
