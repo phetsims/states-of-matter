39b6f3956086015742704c5722e0ed3c3f7c3eb5
chandrashekarBemagoni <chandrashekar.bemagoni@gmail.com>
2015-02-16 19:58:10 +0530

    Restricted temperatureScaleFactor  to preserve molecules rotational energy. See  #28

--- js/common/model/engine/kinetic/IsokineticThermostat.js
+++ js/common/model/engine/kinetic/IsokineticThermostat.js
@@ -54,7 +54,7 @@ define( function( require ) {
       else {
         for ( i = 0; i < this.moleculeDataSet.getNumberOfMolecules(); i++ ) {
           // For single-atom molecules, exclude rotational inertia from the calculation.
-          centersOfMassKineticEnergy += 0.5 * this.moleculeDataSet.moleculeMass *
+          centersOfMassKineticEnergy += 0.5 * this.moleculeDataSet.getMoleculeMass() *
                                         ( Math.pow( this.moleculeVelocities[ i ].x, 2 ) +
                                           Math.pow( this.moleculeVelocities[ i ].y, 2 ) );
         }
@@ -72,9 +72,14 @@ define( function( require ) {
     adjustMeasuredTemperature: function( measuredTemperature ) {
 
       // Calculate the scaling factor that will be used to adjust the temperature.
-      var temperatureScaleFactor = ( this.targetTemperature <= this.minModelTemperature ) ? 0 :
-                                   Math.sqrt( this.targetTemperature / measuredTemperature );
-      // Adjust the temperature by scaling the velocity of each molecule by the appropriate amount.
+      var temperatureScaleFactor;
+      if ( this.targetTemperature <= this.minModelTemperature ) {
+        temperatureScaleFactor = 0;
+      }
+      else {
+        temperatureScaleFactor = Math.sqrt( this.targetTemperature / measuredTemperature );
+      }
+      temperatureScaleFactor = temperatureScaleFactor < 0.9 ? 0.9 : temperatureScaleFactor;
       for ( var i = 0; i < this.moleculeDataSet.getNumberOfMolecules(); i++ ) {
         this.moleculeVelocities[ i ].setXY( this.moleculeVelocities[ i ].x * temperatureScaleFactor,
           this.moleculeVelocities[ i ].y * temperatureScaleFactor );
