51c43618584902aeb7734251433f968cb6485b46
chandrashekarBemagoni <chandrashekar.bemagoni@gmail.com>
2015-03-04 16:40:12 +0530

    Applied  the white stroke around the selection 'atom/molecule' in panel to match the mock up..

--- view/AtomicInteractionsControlPanel.js
+++ view/AtomicInteractionsControlPanel.js
@@ -239,7 +239,7 @@ define( function( require ) {
         baseColor: 'black',
         disabledBaseColor: 'black',
         selectedLineWidth: 1,
-        selectedStroke: '#FFFCD3',
+        selectedStroke: 'white',
         deselectedLineWidth: 0,
         deselectedContentOpacity: 1
       } );
