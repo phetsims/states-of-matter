5db96db57f33103f1c66d0445a33a2ecf02b5999
jbphet <john.blanco@colorado.edu>
2015-03-10 13:40:56 -0600

    Moved the js/atomic-interactions directory from the atomic-interactions repo into a separate repo in preparation for moving it to the states-of-matter repo.

---
+++ js/atomic-interactions/AtomicInteractionsScreen.js
@@ -0,0 +1,44 @@
+//  Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ *
+ * @author John Blanco
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var DualAtomModel = require( 'ATOMIC_INTERACTIONS/atomic-interactions/model/DualAtomModel' );
+  var AtomicInteractionsScreenView = require( 'ATOMIC_INTERACTIONS/atomic-interactions/view/AtomicInteractionsScreenView' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Screen = require( 'JOIST/Screen' );
+  var AtomicInteractionColors = require( 'ATOMIC_INTERACTIONS/atomic-interactions/view/AtomicInteractionColors' );
+  var Image = require( 'SCENERY/nodes/Image' );
+
+  // images
+  var atomicInteractionScreenIcon = require( 'image!STATES_OF_MATTER/som-atomic-interactions-icon.png' );
+
+  function AtomicInteractionsScreen( enableHeterogeneousMolecules, simTitle, colorsProperty ) {
+
+    //If this is a single-screen sim, then no icon is necessary.
+    //If there are multiple screens, then the icon must be provided here.
+    var screen = this;
+    Screen.call( this, simTitle, new Image( atomicInteractionScreenIcon ),
+      function() { return new DualAtomModel(); },
+      function( model ) { return new AtomicInteractionsScreenView( model, enableHeterogeneousMolecules ); },
+      { backgroundColor: AtomicInteractionColors.background.toCSS() }
+    );
+
+    colorsProperty.link( function( color ) {
+      if ( color ) {
+        AtomicInteractionColors.applyProfile( 'projector' );
+      }
+      else {
+        AtomicInteractionColors.applyProfile( 'default' );
+      }
+    } );
+    AtomicInteractionColors.linkAttribute( 'background', screen, 'backgroundColor' );
+  }
+
+  return inherit( Screen, AtomicInteractionsScreen );
+} );
---
+++ js/atomic-interactions/view/DimensionalArrowNode.js
@@ -0,0 +1,78 @@
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Path = require( 'SCENERY/nodes/Path' );
+  var ArrowShape = require( 'SCENERY_PHET/ArrowShape' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  /*
+   * @param {Number} tailX - arrowNode tail X position
+   * @param {Number} tailY - arrowNode tail Y position
+   * @param {Number} tipX - arrowNode tip X position
+   * @param {Number} tipY - arrowNode tip Y position
+   * @param {Object} options that can be passed on to the underlying node
+   * @constructor
+   */
+  function DimensionalArrowNode( tailX, tailY, tipX, tipY, options ) {
+
+    // default options
+    options = _.extend( {
+      headHeight: 10,
+      headWidth: 10,
+      tailWidth: 5,
+      doubleHead: false, // true puts heads on both ends of the arrow, false puts a head at the tip
+      fill: 'black',
+      stroke: 'black',
+      lineWidth: 1
+    }, options );
+    // arrowNode tip and tail locations
+    this.tailLocation = new Vector2( 0, 0 );
+    this.tipLocation = new Vector2( 0, 0 );
+    // things you're likely to mess up, add more as needed
+    assert && assert( options.headWidth > options.tailWidth );
+
+    Path.call( this, new ArrowShape( tailX, tailY, tipX, tipY, options ), options );
+    this.options = options;
+  }
+
+  return inherit( Path, DimensionalArrowNode, {
+    /**
+     *
+     * @param {Number} tailX - tail X position
+     * @param {Number} tailY - tail Y position
+     * @param {Number} tipX - tip X position
+     * @param {Number} tipY - tip Y position
+     */
+    setTailAndTip: function( tailX, tailY, tipX, tipY ) {
+      this.tailLocation.setXY( tailX, tailY );
+      this.tipLocation.setXY( tipX, tipY );
+      var tempHeadHeight;
+      var tempHeadWidth;
+      var tempTailWidth;
+      if ( this.tailLocation.distance( this.tipLocation ) !== 0 ) {
+
+        tempHeadHeight = this.options.headHeight;
+        tempHeadWidth = this.options.headWidth;
+        tempTailWidth = this.options.tailWidth;
+        var length = this.tipLocation.distance( this.tailLocation );
+
+        var fractionalHeadHeight = 0.5;
+        if ( length < this.options.headHeight / fractionalHeadHeight ) {
+          tempHeadHeight = length * fractionalHeadHeight;
+
+          tempTailWidth = this.options.tailWidth * tempHeadHeight / this.options.headHeight;
+          tempHeadWidth = this.options.headWidth * tempHeadHeight / this.options.headHeight;
+
+        }
+      }
+      this.shape = new ArrowShape( tailX, tailY, tipX, tipY, {
+        headHeight: tempHeadHeight,
+        headWidth: tempHeadWidth,
+        tailWidth: tempTailWidth
+      } );
+    }
+  } );
+} );
+
