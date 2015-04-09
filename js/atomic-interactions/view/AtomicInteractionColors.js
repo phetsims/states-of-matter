fa88ad9b325b7c9a98d20802ff6d1ef4c3b42197
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:26 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/atomic-interactions/view/AtomicInteractionColors.js
+++ js/atomic-interactions/view/AtomicInteractionColors.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2015, University of Colorado Boulder
+// Copyright 2002-2015, University of Colorado Boulder
 
 /**
  * An object that contains the colors used for various major components of the Atomic Interactions simulation.  This
