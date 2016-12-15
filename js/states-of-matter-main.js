aa36215a2102f1f96314d6539d4589ca8c10c461
jbphet <john.blanco@colorado.edu>
2016-12-15 11:48:05 -0700

    updated QA credits, see #200
    (cherry picked from commit 00de1b8c00b2e105de3f8691f2c9186cb75fa3e7)

--- js/states-of-matter-main.js
+++ js/states-of-matter-main.js
@@ -29,7 +29,7 @@ define( function( require ) {
       leadDesign: 'Paul Beale, Yuen-ying Carpenter, Sarah McKagan, Emily Moore,\nNoah Podolefsky, Amy Rouinfar',
       softwareDevelopment: 'John Blanco, Aaron Davis, Aadish Gupta',
       team: 'Wendy Adams, Jack Barbera, Amy Hanson, Kelly Lancaster, Ariel Paul, Kathy Perkins',
-      qualityAssurance: 'Steele Dalton, Amanda Davis, Oliver Orejola, Benjamin Roberts,\nBryan Yoelin',
+      qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Ethan Johnson,\nOliver Orejola, Benjamin Roberts, Bryan Yoelin',
       thanks: 'Thanks to Actual Concepts for working with the PhET development team\nto convert this simulation to HTML5.'
     },
     optionsNode: new GlobalOptionsNode( projectorModeProperty ),
