7e48676dcb4810ba355cc68d898c74da1d5ced05
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-18 14:21:44 -0700

    Update copyright year, see https://github.com/phetsims/tasks/issues/988

--- js/common/view/DialGaugeNode.js
+++ js/common/view/DialGaugeNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2018, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * This class represents a node that displays a dial gauge, which is a circular instrument that can be used to portray
