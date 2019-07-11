57880c38db1a175eee63e7204361c28f6afb024f
zepumph <michael.kauzmann@colorado.edu>
2019-07-11 12:54:06 -0800

    update copyright dates, https://github.com/phetsims/tasks/issues/988

--- js/atomic-interactions/view/ForcesControlPanel.js
+++ js/atomic-interactions/view/ForcesControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2018, University of Colorado Boulder
+// Copyright 2015-2019, University of Colorado Boulder
 
 /**
  * This class displays a control panel for controlling the display of attractive, repulsive, and total force.
--- js/atomic-interactions/view/ZoomableGridNode.js
+++ js/atomic-interactions/view/ZoomableGridNode.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2018, University of Colorado Boulder
+// Copyright 2015-2019, University of Colorado Boulder
 
 /**
  * Scenery node that shows the grid lines.  Highly leveraged from energy-skate-park's GridNode implementation.
