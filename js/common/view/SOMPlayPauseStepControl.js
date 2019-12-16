0d87c5e23ffbeaf33fb04f2b2b8c05833e8645e7
Saurabh Totey <SaurabhTotey@gmail.com>
2019-12-15 18:07:33 -0700

    Migrated SOMPlayPauseStepControl to use TimeControlNode
    See #246

--- js/common/view/SOMPlayPauseStepControl.js
+++ js/common/view/SOMPlayPauseStepControl.js
@@ -7,12 +7,10 @@ define( require => {
   'use strict';
 
   // modules
-  const inherit = require( 'PHET_CORE/inherit' );
-  const Node = require( 'SCENERY/nodes/Node' );
-  const PlayPauseButton = require( 'SCENERY_PHET/buttons/PlayPauseButton' );
   const SOMConstants = require( 'STATES_OF_MATTER/common/SOMConstants' );
+  const merge = require( 'PHET_CORE/merge' );
   const statesOfMatter = require( 'STATES_OF_MATTER/statesOfMatter' );
-  const StepForwardButton = require( 'SCENERY_PHET/buttons/StepForwardButton' );
+  const TimeControlNode = require ( 'SCENERY_PHET/TimeControlNode' );
 
   // constants
   const TOUCH_AREA_DILATION = 2.5;
@@ -20,41 +18,38 @@ define( require => {
   const FILL = '#005566';
   const PAUSE_SIZE_INCREASE_FACTOR = 1.25;
 
-  /**
-   * @param {Property.<boolean>} playingProperty
-   * @param {function} stepFunction
-   * @param {Object} options
-   * @constructor
-   */
-  function SOMPlayPauseStepControl( playingProperty, stepFunction, options ) {
+  class SOMPlayPauseStepControl extends TimeControlNode {
 
-    Node.call( this );
+    /**
+     * @param {Property.<boolean>} playingProperty
+     * @param {function} stepFunction
+     * @param {Object} options
+     * @constructor
+     */
+    constructor( playingProperty, stepFunction, options ) {
 
-    const playPauseButton = new PlayPauseButton( playingProperty, {
-      radius: 18,
-      playButtonScaleFactor: PAUSE_SIZE_INCREASE_FACTOR,
-      stroke: STROKE,
-      fill: FILL,
-      touchAreaDilation: TOUCH_AREA_DILATION
-    } );
-    this.addChild( playPauseButton );
+      options = merge( {
+        playPauseOptions: {
+          radius: 18,
+          playButtonScaleFactor: PAUSE_SIZE_INCREASE_FACTOR,
+          stroke: STROKE,
+          fill: FILL,
+          touchAreaDilation: TOUCH_AREA_DILATION
+        },
+        stepForwardOptions: {
+          isPlayingProperty: playingProperty,
+          listener: () => { stepFunction( SOMConstants.NOMINAL_TIME_STEP ); },
+          radius: 12,
+          stroke: STROKE,
+          fill: FILL,
+          touchAreaDilation: TOUCH_AREA_DILATION
+        }
+      }, options );
 
-    const stepButton = new StepForwardButton( {
-      isPlayingProperty: playingProperty,
-      listener: function() { stepFunction( SOMConstants.NOMINAL_TIME_STEP ); },
-      radius: 12,
-      stroke: STROKE,
-      fill: FILL,
-      touchAreaDilation: TOUCH_AREA_DILATION,
-      y: playPauseButton.centerY,
-      left: playPauseButton.right + 10
-    } );
-    this.addChild( stepButton );
+      super( playingProperty, options );
+    }
 
-    this.mutate( options );
   }
 
-  statesOfMatter.register( 'SOMPlayPauseStepControl', SOMPlayPauseStepControl );
-
-  return inherit( Node, SOMPlayPauseStepControl );
+  return statesOfMatter.register( 'SOMPlayPauseStepControl', SOMPlayPauseStepControl );
 } );
