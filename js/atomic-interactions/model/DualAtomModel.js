befe2ec41f410f8893563910798718ea9b1cb318
phet-dev <phetadmin@gmail.com>
2020-01-12 03:48:08 -0700

    update copyright dates from daily grunt work

--- js/atomic-interactions/model/DualAtomModel.js
+++ js/atomic-interactions/model/DualAtomModel.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * This is the model for two atoms interacting with a Lennard-Jones interaction potential.
