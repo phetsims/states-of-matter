f93ccb1d1eb0350a76b3fdd8e7ce596d95934945
phet-dev <phetadmin@gmail.com>
2020-01-25 03:39:29 -0700

    update copyright dates from daily grunt work

--- js/common/model/engine/AbstractPhaseStateChanger.js
+++ js/common/model/engine/AbstractPhaseStateChanger.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * This is the base class for the objects that directly change the state of the molecules within the multi-particle
