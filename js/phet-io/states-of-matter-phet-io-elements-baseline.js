67a9d96636d81cb7a7c6caa97948d754434deaa9
chrisklus <christopher.klusendorf@colorado.edu>
2020-02-18 14:32:01 -0500

    Update phetioDocumentation, see https://github.com/phetsims/joist/issues/591

--- js/phet-io/states-of-matter-phet-io-elements-baseline.js
+++ js/phet-io/states-of-matter-phet-io-elements-baseline.js
@@ -5605,7 +5605,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<ScreenIO>"
     },
     "statesOfMatter.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
+      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether sound is enabled or disabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
