55abb0bae35c746bc0e265a8c8d9e7d586367627
zepumph <michael.kauzmann@colorado.edu>
2020-04-09 11:33:14 -0800

    fix ReferenceIO typeName and phetioDocumentation, https://github.com/phetsims/tandem/issues/159#event-3218972885

--- js/phet-io/states-of-matter-phet-io-types.js
+++ js/phet-io/states-of-matter-phet-io-types.js
@@ -2050,13 +2050,13 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "supertype": "ObjectIO",
       "typeName": "RangeIO"
     },
-    "ReferenceIO": {
-      "documentation": "Uses reference identity for toStateObject/fromStateObject and validates based on its parameter IO Type.",
+    "ReferenceIO<ObjectIO>": {
+      "documentation": "Uses reference identity for serializing and deserializing, and validates based on its parameter IO Type.",
       "events": [],
       "methodOrder": [],
       "methods": {},
       "supertype": "ObjectIO",
-      "typeName": "ReferenceIO"
+      "typeName": "ReferenceIO<ObjectIO>"
     },
     "RichTextIO": {
       "documentation": "The tandem IO type for the scenery RichText node",
@@ -2090,7 +2090,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
-      "supertype": "ReferenceIO",
+      "supertype": "ReferenceIO<ObjectIO>",
       "typeName": "ScreenIO"
     },
     "SliderIO": {
