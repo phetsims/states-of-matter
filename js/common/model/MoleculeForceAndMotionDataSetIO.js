6dd795d7e2b7a7d5c0996b5554a13cac7cbb4f67
samreid <reids@colorado.edu>
2020-09-17 20:59:13 -0600

    Convert to IOType, see https://github.com/phetsims/tandem/issues/211

--- js/common/model/MoleculeForceAndMotionDataSetIO.js
+++ js/common/model/MoleculeForceAndMotionDataSetIO.js
@@ -8,30 +8,15 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import validate from '../../../../axon/js/validate.js';
-import ObjectIO from '../../../../tandem/js/types/ObjectIO.js';
+import IOType from '../../../../tandem/js/types/IOType.js';
 import statesOfMatter from '../../statesOfMatter.js';
 import MoleculeForceAndMotionDataSet from './MoleculeForceAndMotionDataSet.js';
 
-class MoleculeForceAndMotionDataSetIO extends ObjectIO {
-
-  /**
-   * Encodes a MoleculeForceAndMotionDataSet instance to a state.
-   * @param {MoleculeForceAndMotionDataSet} moleculeForceAndMotionDataSet
-   * @returns {Object}
-   * @public
-   * @override
-   */
-  static toStateObject( moleculeForceAndMotionDataSet ) {
-    validate( moleculeForceAndMotionDataSet, this.validator );
-    return moleculeForceAndMotionDataSet.toStateObject();
-  }
-}
-
-MoleculeForceAndMotionDataSetIO.documentation = 'particle data set';
-MoleculeForceAndMotionDataSetIO.validator = { valueType: MoleculeForceAndMotionDataSet };
-MoleculeForceAndMotionDataSetIO.typeName = 'MoleculeForceAndMotionDataSetIO';
-ObjectIO.validateIOType( MoleculeForceAndMotionDataSetIO );
+const MoleculeForceAndMotionDataSetIO = new IOType( 'MoleculeForceAndMotionDataSetIO', {
+  valueType: MoleculeForceAndMotionDataSet,
+  documentation: 'particle data set',
+  toStateObject: moleculeForceAndMotionDataSet => moleculeForceAndMotionDataSet.toStateObject()
+} );
 
 statesOfMatter.register( 'MoleculeForceAndMotionDataSetIO', MoleculeForceAndMotionDataSetIO );
 export default MoleculeForceAndMotionDataSetIO;
--- js/common/model/MultipleParticleModelIO.js
+++ js/common/model/MultipleParticleModelIO.js
@@ -1,57 +1,15 @@
 // Copyright 2020, University of Colorado Boulder
 
-import validate from '../../../../axon/js/validate.js';
-import ObjectIO from '../../../../tandem/js/types/ObjectIO.js';
+import IOType from '../../../../tandem/js/types/IOType.js';
 import statesOfMatter from '../../statesOfMatter.js';
 import MultipleParticleModel from './MultipleParticleModel.js';
 
-/**
- * IO Type for MultipleParticleModel
- *
- * @author John Blanco (PhET Interactive Simulations)
- * @author Michael Kauzmann (PhET Interactive Simulations)
- */
-class MultipleParticleModelIO extends ObjectIO {
-
-  /**
-   * serialized a MultipleParticleModel instance
-   * @param {MultipleParticleModel} multipleParticleModel
-   * @returns {Object}
-   * @public
-   * @override
-   */
-  static toStateObject( multipleParticleModel ) {
-    validate( multipleParticleModel, this.validator );
-    return multipleParticleModel.toStateObject();
-  }
-
-  /**
-   * Deserialize component parts into an intermediate object that can be set in applyState.
-   * @param {Object} stateObject
-   * @returns {Object}
-   * @public
-   * @override
-   */
-  static fromStateObject( stateObject ) {
-    return MultipleParticleModel.fromStateObject( stateObject );
-  }
-
-  /**
-   * set the state of a MultipleParticleModel instance
-   * @param {MultipleParticleModel} multipleParticleModel
-   * @param {Object} state
-   * @public
-   * @override
-   */
-  static applyState( multipleParticleModel, state ) {
-    multipleParticleModel.applyState( state );
-  }
-}
-
-MultipleParticleModelIO.documentation = 'multiple particle model that simulates interactions that lead to phase-like behavior';
-MultipleParticleModelIO.validator = { isValidValue: v => v instanceof MultipleParticleModel };
-MultipleParticleModelIO.typeName = 'MultipleParticleModelIO';
-ObjectIO.validateIOType( MultipleParticleModelIO );
+const MultipleParticleModelIO = new IOType( 'MultipleParticleModelIO', {
+  isValidValue: v => v instanceof MultipleParticleModel,
+  documentation: 'multiple particle model that simulates interactions that lead to phase-like behavior',
+  toStateObject: multipleParticleModel => multipleParticleModel.toStateObject(),
+  applyState: ( multipleParticleModel, state ) => multipleParticleModel.applyState( state )
+} );
 
 statesOfMatter.register( 'MultipleParticleModelIO', MultipleParticleModelIO );
 export default MultipleParticleModelIO;
