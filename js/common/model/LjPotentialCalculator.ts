a8b000180429b29cf3859e06fed01e7283627a06
Sam Reid <reids@colorado.edu>
2025-08-08 09:38:11 -0600

    Convert to TypeScript, see https://github.com/phetsims/states-of-matter/issues/368

--- js/common/model/LjPotentialCalculator.ts
+++ js/common/model/LjPotentialCalculator.ts
@@ -16,11 +16,7 @@ import SOMConstants from '../SOMConstants.js';
  */
 class LjPotentialCalculator {
 
-  /**
-   * @param {number} sigma
-   * @param {number} epsilon
-   */
-  constructor( sigma, epsilon ) {
+  constructor( sigma: number, epsilon: number ) {
 
     assert && assert( sigma > 0, 'sigma must be greater than 0' );
 
@@ -29,77 +25,57 @@ class LjPotentialCalculator {
     this.epsilonForCalcs = this.epsilon * SOMConstants.K_BOLTZMANN;  // Epsilon multiplied by k-boltzmann.
   }
 
-  /**
-   * @returns {number}
-   * @public
-   */
-  getSigma() {
+  public getSigma(): number {
     return this.sigma;
   }
 
-  /**
-   * @param {number} sigma
-   * @public
-   */
-  setSigma( sigma ) {
+  public setSigma( sigma: number ) {
     this.sigma = sigma;
   }
 
-  /**
-   * @returns {number}
-   * @public
-   */
-  getEpsilon() {
+  public getEpsilon(): number {
     return this.epsilon;
   }
 
-  /**
-   * @param {number} epsilon
-   * @public
-   */
-  setEpsilon( epsilon ) {
+  public setEpsilon( epsilon: number ) {
     this.epsilon = epsilon;
     this.epsilonForCalcs = this.epsilon * SOMConstants.K_BOLTZMANN;
   }
 
   /**
    * Calculate the Lennard-Jones potential for the specified distance.
-   * @param {number} distance - Distance between interacting molecules in picometers.
-   * @returns {number} Strength of the potential in newton-meters (N*m).
-   * @public
+   * @param distance - Distance between interacting molecules in picometers.
+   * @returns Strength of the potential in newton-meters (N*m).
    */
-  getLjPotential( distance ) {
+  public getLjPotential( distance: number ): number {
     const distanceRatio = this.sigma / distance;
     return ( 4 * this.epsilonForCalcs * ( Math.pow( distanceRatio, 12 ) - Math.pow( distanceRatio, 6 ) ) );
   }
 
   /**
    * Calculate only the repulsive component of the Lennard-Jones force for the specified distance.
-   * @param {number} distance - Distance between interacting molecules in picometers.
-   * @returns {number} Force in newtons.
-   * @public
+   * @param distance - Distance between interacting molecules in picometers.
+   * @returns Force in newtons.
    */
-  getRepulsiveLjForce( distance ) {
+  public getRepulsiveLjForce( distance: number ): number {
     return ( 48 * this.epsilonForCalcs * Math.pow( this.sigma, 12 ) / Math.pow( distance, 13 ) );
   }
 
   /**
    * Calculate only the attractive component of the Lennard-Jones force for the specified distance.
-   * @param {number} distance - Distance between interacting molecules in picometers.
-   * @returns {number} - Force in newtons.
-   * @public
+   * @param distance - Distance between interacting molecules in picometers.
+   * @returns Force in newtons.
    */
-  getAttractiveLjForce( distance ) {
+  public getAttractiveLjForce( distance: number ): number {
     return ( 24 * this.epsilonForCalcs * Math.pow( this.sigma, 6 ) / Math.pow( distance, 7 ) );
   }
 
   /**
    * Calculate the distance at which the force is 0 because the attractive and repulsive forces are balanced.  Note
    * that this is where the potential energy is at a minimum.
-   * @returns {number} - Distance where force is 0 (or very close) in picometers.
-   * @public
+   * @returns Distance where force is 0 (or very close) in picometers.
    */
-  getMinimumForceDistance() {
+  public getMinimumForceDistance(): number {
 
     // this is the solution for the min potential
     return this.sigma * Math.pow( 2, 1 / 6 );
