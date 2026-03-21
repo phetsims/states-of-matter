feca9d325649adf8c24bf90f1e062c91eb1e5f74
phet-dev <phetadmin@gmail.com>
2026-03-21 04:55:51 -0600

    update copyright dates from daily grunt work

--- js/atomic-interactions/view/PushPinNode.ts
+++ js/atomic-interactions/view/PushPinNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2025, University of Colorado Boulder
+// Copyright 2015-2026, University of Colorado Boulder
 
 /**
  * PushPinNode represents the pushpin used to "pin" an atom in one position.
